e55689157f039bf58a28e7ebcf67e90a21583c03
mcclellansws <mcclellan.sws@gmail.com>
2019-11-26 21:17:19 -0600

    half theme

--- themes.js
+++ themes.js
@@ -3,24 +3,25 @@
 
 var today = new Date();
 var expiry = new Date(today.getTime() + 365 * 24 * 60 * 60 * 1000);
+const session = require('electron').remote.session.defaultSession;
 
 function getCookieVal (offset) {
-	var endstr = document.cookie.indexOf (";", offset);
-	if (endstr == -1) { endstr = document.cookie.length; }
-	return unescape(document.cookie.substring(offset, endstr));
+	var endstr = session.cookie.indexOf (";", offset);
+	if (endstr == -1) { endstr = session.cookie.length; }
+	return unescape(session.cookie.substring(offset, endstr));
 	}
 
 function GetCookie (name) {
 	var arg = name + "=";
 	var alen = arg.length;
-	var clen = document.cookie.length;
+	var clen = session.cookie.length;
 	var i = 0;
 	while (i < clen) {
 		var j = i + alen;
-		if (document.cookie.substring(i, j) == arg) {
+		if (session.cookie.substring(i, j) == arg) {
 			return getCookieVal (j);
 			}
-		i = document.cookie.indexOf(" ", i) + 1;
+		i = session.cookie.indexOf(" ", i) + 1;
 		if (i == 0) break; 
 		}
 	return null;
@@ -28,7 +29,7 @@ function GetCookie (name) {
 
 function DeleteCookie (name,path,domain) {
 	if (GetCookie(name)) {
-		document.cookie = name + "=" +
+		session.cookie = name + "=" +
 		((path) ? "; path=" + path : "") +
 		((domain) ? "; domain=" + domain : "") +
 		"; expires=Thu, 01-Jan-70 00:00:01 GMT";
@@ -36,7 +37,7 @@ function DeleteCookie (name,path,domain) {
 	}
 
 function SetCookie (name,value,expires,path,domain,secure) {
-  document.cookie = name + "=" + escape (value) +
+	session.cookie = name + "=" + escape (value) +
     ((expires) ? "; expires=" + expires.toGMTString() : "") +
     ((path) ? "; path=" + path : "") +
     ((domain) ? "; domain=" + domain : "") +
@@ -51,19 +52,22 @@ var THEME_KEY = "haf_theme";	// this key is used to store the selected theme.
 var THEMES_FOLDER_PATH = "./css/";
 var DEFAULT_THEME = "theme_default";
 
-function insertStyleSheet() {
+function updateTheme() {
 	var s = GetCookie(THEME_KEY);
 	if (s == null) s = DEFAULT_THEME;
-	document.writeln('<link href="'+THEMES_FOLDER_PATH+s+'.css" type="text/css" rel="stylesheet">');
+	var t = document.getElementById('theme');
+	t.setAttribute('href', THEMES_FOLDER_PATH + s + '.css'); 
+	// document.writeln('<link href="'+THEMES_FOLDER_PATH+s+'.css" type="text/css" rel="stylesheet">');
 }
 
 function setTheme() {
 	DeleteCookie(THEME_KEY);					
 	SetCookie(THEME_KEY, document.getElementById('selectTheme').value);
-	self.location = self.location;	// simple trick to reload the current document
+	// self.location = self.location;	// simple trick to reload the current document
+	updateTheme();
 }
 	
-insertStyleSheet();
+updateTheme();
 	
 
 
